6330cf876d865a7adb58edae744ea1bb47881e35
EVANDRO QUEIROZ ARAUJO <101594488+evandroqa@users.noreply.github.com>
2024-12-09 12:18:37 -0300

    "Projeto criado"

---
+++ src/main/java/com/cursomc/UdemyUML/UdemyUmlApplication.java
@@ -0,0 +1,13 @@
+package com.cursomc.UdemyUML;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyUmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyUmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursomc/UdemyUML/UdemyUmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursomc.UdemyUML;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyUmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
