0ff7c4e1fed12a84dab3df4875975886c904e3bc
Evandro Araújo <evandroqa@gmail.com>
2024-12-09 21:52:22 -0300

    Banco de dado H2

--- src/main/java/com/cursomc/entities/Categoria.java
+++ src/main/java/com/cursomc/entities/Categoria.java
@@ -2,14 +2,22 @@ package com.cursomc.entities;
 
 import java.util.Objects;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_categoria")
 public class Categoria {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(columnDefinition = "TEXT")
 	private String nome;
 	
 	public Categoria() {
